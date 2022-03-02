3e1dcb57634b4d5ead2d9d788fccc688eb5b7dc4
马金龙 <930079179@qq.com>
2022-03-02 10:24:10 +0800

    1.0.0+2
    1.0.0+2

--- lib/src/TipUtil.dart
+++ lib/src/TipUtil.dart
@@ -5,7 +5,7 @@ class TipUtil {
   static void showToast(String msg, {backgroundColor, textColor}) {
     if (!ObjectUtil.isEmptyString(msg)) {
       Fluttertoast.showToast(msg: "$msg",
-          toastLength: Toast.LENGTH_LONG,
+          toastLength: Toast.LENGTH_SHORT,
           gravity: ToastGravity.CENTER,
           timeInSecForIosWeb: 1,
           backgroundColor: backgroundColor,
